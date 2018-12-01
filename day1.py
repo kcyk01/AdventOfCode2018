3b10836fd40034afa580c90bd54fffb5c4c00095
unknown <kcyk01@yahoo.ca>
2018-12-01 00:23:24 -0500

    Day 1 solutions

--- day1.py
+++ day1.py
@@ -1,12 +1,20 @@
 from utils import get_input
 
 
-def day1a(puzzle_input: str):
-    pass
+def day1a(puzzle_input: str) -> int:
+    return sum([int(num) for num in puzzle_input.strip().split('\n')])
 
 
-def day1b(puzzle_input: str):
-    pass
+def day1b(puzzle_input: str) -> int:
+    nums = [int(num) for num in puzzle_input.strip().split('\n')]
+    freq_set = set()
+    freq = i = 0
+    while True:
+        freq += int(nums[i])
+        if freq in freq_set:
+            return freq
+        freq_set.add(freq)
+        i = (i + 1) % len(nums)
 
 
 if __name__ == '__main__':
